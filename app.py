0e8931900c4f0e3b4fcab5edc86fdc52399348ce
ArjunYaduvanshi02 <sidyaduvanshi02@gmail.com>
2024-05-22 12:59:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import flask
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 @app.route('/')
 def start():
-    return "meow meow nigga"
+    return render_template('index.html')
