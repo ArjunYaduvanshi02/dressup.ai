918935e902717e092201f29b92e573095a19b592
ArjunYaduvanshi02 <sidyaduvanshi02@gmail.com>
2024-05-23 10:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def start():
+    return render_template('opening.html')
+@app.route('/read_more', methods=['GET', 'POST'])
+def read_more_func():
     return render_template('index.html')
 @app.route('/contact_us', methods=['GET', 'POST'])
 def contact_us():
