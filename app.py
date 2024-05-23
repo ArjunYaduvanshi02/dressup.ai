5b42e769c2d9c7c771a0788bb1a53f42bfc14e36
ArjunYaduvanshi02 <sidyaduvanshi02@gmail.com>
2024-05-23 10:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def start():
+    return render_template('index.html')
+@app.route('/contact_us', methods=['GET', 'POST'])
+def contact_us():
+    return render_template('feedback.html')
+@app.route('/startup', methods=['GET', 'POST'])
+def startup():
     return render_template('opening.html')
